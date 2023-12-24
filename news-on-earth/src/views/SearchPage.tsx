1f9b29e138b5be6ab618a5d99ca2f54f9f4a99ae
Ai-TM <ai.wana.b.wi0.u@gmail.com>
2023-12-24 11:12:08 +0000

    feat: import SearchBox to SearchPage

--- news-on-earth/src/views/SearchPage.tsx
+++ news-on-earth/src/views/SearchPage.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import SearchBox from './SearchBox';
 
 const SearchPage: React.FC = () => {
+  const handleSearch = (searchTerm: string) => {
+    console.log('検索語:', searchTerm);
+    // API呼び出し
+  };
+  
   return (
     <div>
       <h1>ここに検索ページを書きます。</h1>
+      <SearchBox onSearchChange={handleSearch} />
     </div>
   );
 };
