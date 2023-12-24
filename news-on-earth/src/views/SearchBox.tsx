769524de0e7a87b9c68a4f8b7dc64c7e786d23fc
Ai-TM <ai.wana.b.wi0.u@gmail.com>
2023-12-24 11:49:17 +0000

    fix: indent

--- news-on-earth/src/views/SearchBox.tsx
+++ news-on-earth/src/views/SearchBox.tsx
@@ -1,31 +1,33 @@
 import React, { useState, ChangeEvent } from 'react';
 
 interface SearchBoxProps {
-  onSearchChange: (searchTerm: string) => void;
+	onSearchChange: (searchTerm: string) => void;
 }
 
 const SearchBox: React.FC<SearchBoxProps> = ({ onSearchChange }) => {
-  const [searchTerm, setSearchTerm] = useState<string>('');
+	const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value); //入力変更をリアルタイム検知
-  };
+	const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchTerm(e.target.value); //入力変更をリアルタイム検知
+	};
 
-  const handleButtonClick = () => {
-    onSearchChange(searchTerm); // ボタンクリック時に検索処理を実行
-  };
+	const handleButtonClick = () => {
+		onSearchChange(searchTerm); // ボタンクリック時に検索処理を実行
+	};
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="日本語でキーワードを入力"
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
-      <button onClick={handleButtonClick}>検索</button> 
-    </div>
-  );
+	return (
+		<div>
+			<input
+				type="text"
+				placeholder="日本語でキーワードを入力"
+				value={searchTerm}
+				onChange={handleSearchChange}
+			/>
+			<button onClick={handleButtonClick}>
+
+			</button>
+		</div>
+	);
 };
 
 export default SearchBox;
