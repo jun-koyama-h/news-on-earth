6d629a45e09a1a41bc75e38fabee341cc99a1e56
saga <m.s.1123.aqui@gmail.com>
2023-12-29 12:10:59 +0000

    ロゴを背景に変更

--- news-on-earth/src/views/Logo.tsx
+++ news-on-earth/src/views/Logo.tsx
@@ -2,9 +2,19 @@ import logo from '../assets/logo.svg'; // ロゴファイルをインポート
 
 const Logo = () => {
   return (
-    <div>
-      <img src={logo} alt="Logo" width="500" height="200"/>
-    </div>
+    <div style={{
+      backgroundImage: `url(${logo})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      minHeight: '100vh', // ビューポートの高さに合わせる
+      minWidth: '100vw', // ビューポートの幅に合わせる
+      position: 'fixed', // 固定位置
+      top: 0,
+      left: 0,
+      zIndex: -1, // 他の要素の背景に表示
+      opacity: 0.1 // 透明度を設定
+    }} />
   );
 };
 
