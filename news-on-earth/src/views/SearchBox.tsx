d3fb442160f56ad455ab24a02d0103ca69ec3710
saga <m.s.1123.aqui@gmail.com>
2023-12-29 12:22:58 +0000

    検索ボックスの大きさを変更

--- news-on-earth/src/views/SearchBox.tsx
+++ news-on-earth/src/views/SearchBox.tsx
@@ -26,6 +26,11 @@ const SearchBox: React.FC<SearchBoxProps> = ({ onSearchChange }) => {
 				placeholder="日本語でキーワードを入力"
 				value={searchTerm}
 				onChange={handleSearchChange}
+                style={{
+                    fontSize: '20px', // フォントサイズを大きくする
+                    padding: '10px', // テキストボックス内のパディングを増やす
+                    width: '300px', // テキストボックスの幅を指定する
+                }}
 			/>
 			<button onClick={handleButtonClick}>
 				<img src={searchIcon} alt="検索" width="20" height="20"/>
