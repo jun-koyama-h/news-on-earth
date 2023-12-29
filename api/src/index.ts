16d23756b24da0214b15ac37a3d68b45ec66e286
Jun Koyama <a23z1jk@aiit.ac.jp>
2023-12-29 00:45:49 +0000

    fix: translate.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,47 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { Ai } from '@cloudflare/ai';
 
-const app = new Hono()
+type Bindings = {
+    AI: any;
+};
 
-app.use('/api/*', cors())
+export interface Env {
+    AI: any;
+}
 
-app.get('/', (c) => c.text('Hello News on Earth!'))
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/api/', (c) => c.text('This is api!'))
+app.use('/api/*', cors());
 
-export default app
+app.get('/', (c) => c.text('Hello News on Earth!'));
+
+app.get('/api/', (c) => c.text('This is api!'));
+
+app.post('/api/translate/', async (c) => {
+    try {
+        const requestJson = await c.req.json();
+        if (!requestJson) {
+            return c.text('必須パラメータが見つかりません: text', 400);
+        }
+
+        const ai = new Ai(c.env.AI);
+
+        const translateText = requestJson.translateText
+
+        const result = await ai.run('@cf/meta/m2m100-1.2b', {
+            text: translateText,
+            source_lang: "japanese",
+            target_lang: "english"
+        });
+
+        console.log(JSON.stringify(result));
+
+        return c.text(JSON.stringify(result));
+    } catch (error) {
+        console.log(error);
+        return c.text('エラー', 500);
+    }
+});
+
+export default app;
