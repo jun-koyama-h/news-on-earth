10e051cb9ed462871ce29a8b5377efca0b7b83c9
saga <m.s.1123.aqui@gmail.com>
2024-01-07 13:21:50 +0000

    検索結果を引き渡せるように、検索ページと結果ページを修正

--- news-on-earth/src/views/SearchPage.tsx
+++ news-on-earth/src/views/SearchPage.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // useNavigate をインポート
 import SearchBox from './SearchBox';
 import Background from './Background';
 
@@ -6,10 +7,31 @@ interface TranslationResponse {
   translated_text: string;
 }
 
-const SearchPage: React.FC = () => {
-  const handleSearch = async (searchTerm: string) => {
-    console.log('検索語:', searchTerm);
+interface Article {
+  headline: string;
+  content: string;
+}
 
+const SearchPage: React.FC = () => {
+  const navigate = useNavigate(); // useNavigate フックの初期化
+  const [keyword, setKeyword] = useState<string>('');
+  const handleSearch = async (searchTerm: string) => {
+
+    console.log('検索語:', searchTerm);
+    setKeyword(searchTerm); // keyword 更新
+
+    const hardcodedArticles: Article[] = [
+      {
+        headline: '固定記事の見出し1',
+        content: '固定記事の内容1...',
+      },
+      {
+        headline: '固定記事の見出し2',
+        content: '固定記事の内容2...',
+      },
+      // 他の固定記事
+    ];
+    
     try {
       const response = await fetch('https://api.news-on-earth.workers.dev/api/translate/', {
         method: 'POST',
@@ -25,9 +47,16 @@ const SearchPage: React.FC = () => {
 
       const result: TranslationResponse = await response.json();
       console.log('翻訳結果:', result.translated_text);
+  
     } catch (error) {
       console.error('APIリクエストエラー:', error);
     }
+    navigate('/SearchResult', { 
+      state: { 
+        keyword: searchTerm, // searchTerm または 'サンプル検索キーワード' を渡す
+        articles: hardcodedArticles // 検索結果の固定記事を渡す
+      } 
+    });
   };
 
   return (
