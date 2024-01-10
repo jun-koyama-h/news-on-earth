2c9e38b426cfc18442153b3f76d72a970e15f697
z23018nk <153108806+z23018nk@users.noreply.github.com>
2024-01-10 21:39:38 +0000

    キーワードを全削除の×ボタン表示

--- news-on-earth/src/views/SearchBox.tsx
+++ news-on-earth/src/views/SearchBox.tsx
@@ -2,38 +2,47 @@ import React, { useState, ChangeEvent } from 'react';
 import searchIcon from '../assets/search.svg';
 
 interface SearchBoxProps {
-	onSearchChange: (searchTerm: string) => void;
+    onSearchChange: (searchTerm: string) => void;
 }
 
 const SearchBox: React.FC<SearchBoxProps> = ({ onSearchChange }) => {
-	const [searchTerm, setSearchTerm] = useState<string>('');
+    const [searchTerm, setSearchTerm] = useState<string>('');
 
-	const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setSearchTerm(e.target.value); //入力変更をリアルタイム検知
-	};
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+    };
 
-	const handleButtonClick = () => {
-		onSearchChange(searchTerm); // ボタンクリック時に検索処理を実行
-	};
+    const handleButtonClick = () => {
+        onSearchChange(searchTerm);
+    };
 
-	return (
-		<div>
-			<input
-				type="text"
-				placeholder="日本語でキーワードを入力"
-				value={searchTerm}
-				onChange={handleSearchChange}
+    const handleClearButtonClick = () => {
+        setSearchTerm('');
+    };
+
+    return (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <input
+                type="text"
+                placeholder="日本語でキーワードを入力"
+                value={searchTerm}
+                onChange={handleSearchChange}
                 style={{
-                    fontSize: '20px', // フォントサイズを大きくする
-                    padding: '10px', // テキストボックス内のパディングを増やす
-                    width: '300px', // テキストボックスの幅を指定する
+                    fontSize: '20px',
+                    padding: '10px',
+                    width: '300px',
                 }}
-			/>
-			<button onClick={handleButtonClick}>
-				<img src={searchIcon} alt="検索" width="20" height="20"/>
-			</button>
-		</div>
-	);
+            />
+            {searchTerm && (
+                <button onClick={handleClearButtonClick} style={{ marginLeft: '10px' }}>
+                    ×
+                </button>
+            )}
+            <button onClick={handleButtonClick} style={{ marginLeft: '10px' }}>
+                <img src={searchIcon} alt="検索" width="20" height="20" />
+            </button>
+        </div>
+    );
 };
 
 export default SearchBox;
