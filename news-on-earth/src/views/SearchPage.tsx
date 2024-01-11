55297289ba752a68d02efd397dc0c6dfed4c90fe
Ai-TM <ai.wana.b.wi0.u@gmail.com>
2024-01-11 16:24:20 +0000

    feat: get articles from News API to console

--- news-on-earth/src/views/SearchPage.tsx
+++ news-on-earth/src/views/SearchPage.tsx
@@ -12,6 +12,21 @@ interface TranslationResponse {
   translated_text: string;
 }
 
+interface NewsApiResponse {
+  status: string;
+  totalResults: number;
+  articles: {
+    source: { id: string, name: string };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+  }[];
+}
+
 interface Article {
   headline: string;
   content: string;
@@ -24,6 +39,7 @@ const SearchPage: React.FC = () => {
   const [debounceTimer, setDebounceTimer] = useState<number | null>(null);
   const [searchTerm, setSearchTerm] = useState<string>(''); // 検索ボックスの値を保持するステート
 
+  //サジェストAPI
   const handleSearchChange = async (searchTerm: string) => {
     // 既存のタイマーをクリア
     if (debounceTimer) {
@@ -69,9 +85,17 @@ const SearchPage: React.FC = () => {
 
   };
 
+  // サジェストリストの項目がクリックされたときに呼び出される関数
+  const handleSuggestionClick = (suggestion: string) => {
+    setSearchTerm(suggestion); // 検索ボックスの値を更新
+    handleSearch(suggestion); // 検索処理を実行
+  };
+
+  //翻訳・NewsAPI
   const handleSearch = async (searchTerm: string) => {
     console.log('検索語:', searchTerm);
 
+    //TODO:仮表示のため、削除
     const hardcodedArticles: Article[] = [
       {
         headline: '固定記事の見出し1',
@@ -83,11 +107,12 @@ const SearchPage: React.FC = () => {
         content: '固定記事の内容2...',
         source: 'WSJ',
       },
-      // 他の固定記事
     ];
+    //TODO:仮表示のため、削除
     
     try {
-      const response = await fetch('https://api.news-on-earth.workers.dev/api/translate/', {
+      //翻訳API：キーワードの日→英翻訳
+      const translateResponse = await fetch('https://api.news-on-earth.workers.dev/api/translate/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -95,12 +120,28 @@ const SearchPage: React.FC = () => {
         body: JSON.stringify({ translateText: searchTerm })
       });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      if (!translateResponse.ok) {
+        throw new Error(`HTTP error! status: ${translateResponse.status}`);
       }
 
-      const result: TranslationResponse = await response.json();
-      console.log('翻訳結果:', result.translated_text);
+      const translateResult: TranslationResponse = await translateResponse.json();
+      console.log('翻訳結果:', translateResult.translated_text);
+      
+      //NewsAPI
+      const newsResponse = await fetch('https://api.news-on-earth.workers.dev/api/news/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ q: translateResult.translated_text })
+      });
+
+      if (!newsResponse.ok) {
+        throw new Error(`HTTP error! status: ${newsResponse.status}`);
+      }
+
+      const newsResult: NewsApiResponse = await newsResponse.json();
+      console.log('取得記事:', newsResult.articles);
   
     } catch (error) {
       console.error('APIリクエストエラー:', error);
@@ -113,12 +154,7 @@ const SearchPage: React.FC = () => {
     });
   };
 
-  // サジェストリストの項目がクリックされたときに呼び出される関数
-  const handleSuggestionClick = (suggestion: string) => {
-    setSearchTerm(suggestion); // 検索ボックスの値を更新
-    handleSearch(suggestion); // 検索処理を実行
-  };
-  
+
   return (
     <div className={styles.container}>
       <Logo />
