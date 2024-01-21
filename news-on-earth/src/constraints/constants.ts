bff7d9fe8915e65e2534893bda3a964ac79cdd98
Jun Koyama <a23z1jk@aiit.ac.jp>
2024-01-21 04:20:19 +0000

    bugfix/news-api-url

--- news-on-earth/src/constraints/constants.ts
+++ news-on-earth/src/constraints/constants.ts
@@ -1,6 +1,6 @@
 export const API_TRANSLATE = 'https://api.news-on-earth.workers.dev/api/translate/';
 export const API_SUGGEST = 'https://api.news-on-earth.workers.dev/api/suggest/';
-export const API_NEWS = 'https://api.news-on-earth.workers.dev/api/news//';
+export const API_NEWS = 'https://api.news-on-earth.workers.dev/api/news/';
 
 export const API_HEADERS = {
     'Content-Type': 'application/json',
