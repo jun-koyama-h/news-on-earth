56010dec89e227a5044c2d27d6899fce4051fe91
z23018nk <153108806+z23018nk@users.noreply.github.com>
2024-01-23 12:39:13 +0000

    ロゴを押下時にサーチページへ遷移

--- news-on-earth/src/views/Logo.tsx
+++ news-on-earth/src/views/Logo.tsx
@@ -1,11 +1,18 @@
 import logo from '../assets/logo.svg'; // ロゴファイルをインポート
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 interface LogoProps {
   width: number;
   height: number;
 }
 const Logo: React.FC<LogoProps> = ({ width, height }) => {
+  const navigate = useNavigate();
+  const navigateToSearch = () => {
+    navigate('/SearchPage'); // '/search'はSearchPage.tsxに対応するパス
+  };
   return (
-    <div>
+    <div onClick={navigateToSearch} style={{ cursor: 'pointer' }}>
       <img src={logo} alt="Logo" width={width} height={height}/>
     </div>
   );
